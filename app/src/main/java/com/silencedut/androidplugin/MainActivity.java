c2e673dbe36f95eeb5bd3dc3626c4cf5ee3ce301
SilenceDut <silencedutchman@foxmail.com>
2016-08-03 18:10:03 +0800

    study class loader

--- app/src/main/java/com/silencedut/androidplugin/MainActivity.java
+++ app/src/main/java/com/silencedut/androidplugin/MainActivity.java
@@ -1,13 +1,52 @@
 package com.silencedut.androidplugin;
 
+import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+
+import dalvik.system.DexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String dexOrApkPath = Environment.getExternalStorageDirectory() + "/androidplugin/app.apk";
+
+        Context context=getApplicationContext();//获取Context对象；
+        File dexOutputDir = context.getDir("dex", Context.MODE_PRIVATE);
+        ClassLoader parentClassLoader = ClassLoader.getSystemClassLoader();
+        ClassLoader dexClassLoader = new DexClassLoader(dexOrApkPath, dexOutputDir.getAbsolutePath(), null,parentClassLoader);
+        try {
+            Class cls = dexClassLoader.loadClass("com.silencedut.uirelated.MainActivity");
+            cls.getSimpleName();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (dexClassLoader != null){
+            Log.i(TAG, "[onCreate] dexClassLoader :" + dexClassLoader.toString());
+            while (dexClassLoader.getParent()!=null){
+                dexClassLoader = dexClassLoader.getParent();
+                Log.i(TAG,"[onCreate] dexClassLoader getParent(): " + dexClassLoader.toString());
+            }
+        }
+        ClassLoader classLoader = getClassLoader();
+
+        if (classLoader != null){
+            Log.i(TAG, "[onCreate] classLoader :" + classLoader.toString());
+            while (classLoader.getParent()!=null){
+                classLoader = classLoader.getParent();
+                Log.i(TAG,"[onCreate] classLoader getParent(): " + classLoader.toString());
+            }
+        }
     }
+
 }
